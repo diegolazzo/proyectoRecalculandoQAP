d433765433e8e2a06ed28d75c96eb66769f981c2
diegodev <diego.geo7@gmail.com>
2022-03-26 13:27:32 -0300

    modifiqué el script y realicé comentarios al código

---
+++ script.js
@@ -0,0 +1,13 @@
+// Primer paso: definir el algoritmo para recalcular los porcentajes de QAP al 100%
+
+// Idea: el %Q recalc permite defenir el piso en diagrama ternario (teniendo un abanico de posibilidades) y el recalculado entre AP al 100%
+// permitiria definir la diagonal y obtener el nombre de la muestra
+// Esto permitiría armar un switch que evalúe los rangos y devuelva la muestra
+// Permitiendo mostrar por pantalla el diagrama con resultado resaltado del área donde estaría la muestra, nombre de la muestra y un dato curioso
+
+// Partir con QAP igneo, luego agregar la característica de que se seleccione entre QAP Rx ignea o QAP Rx volcánica extrusiva
+
+// La v.2 escalar a que se puedan en la appweb full (pc) ir almacenando un listado con los nombres de las muestras y poder exportar
+
+// v.3 que se grafique y pueda exportar la posición exacta de la muestra en el diagrama
+// v.4 que se puedan graficar varias muestras y exportar
